59662a4b583421b9727cbc9a9caab402b7a51267
ChillerDragon <ChillerDragon@gmail.com>
2023-04-15 18:27:01 +0200

    find chiler

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # ddnet-bot-irc
 irc bot for ddnet#developer
 
+buy node v18 or later
+
 ping moderators UwU
--- index.js
+++ index.js
@@ -2,16 +2,52 @@ const irc = require('irc')
 const { networkInterfaces } = require('os')
 require('dotenv').config()
 
-const client = new irc.Client('irc.ipv6.quakenet.org', 'chillerbot', {
-	channels: ['#ddnet'],
-})
-
 const interfaces = networkInterfaces()
 const eth0 = interfaces.eth0 ? interfaces.eth0.filter((a) => a.family === 'IPv4')[0].address : ''
 
 console.log(`eth0=${eth0}`)
 
-client.addListener('message#ddnet', (from, message) => {
+const getServerIpsByPlayerName = async (searchName) => {
+	const res = await fetch('https://master1.ddnet.org/ddnet/15/servers.json')
+	const data = await res.json()
+	let matchedEntries = []
+	data.servers.forEach((entry) => {
+		const names = entry.info.clients.map((client) => client.name)
+		if (names.includes(searchName)) {
+			matchedEntries.push(entry)
+		}
+	})
+	console.log(matchedEntries)
+	matchedEntries = matchedEntries.filter((e) => e.info.name.startsWith('DDNet '))
+
+	console.log(matchedEntries)
+	const ips = []
+	matchedEntries.forEach((entry) => {
+		entry.addresses
+			.filter((addr) => addr.startsWith('tw-0.7+udp://'))
+			.forEach((addr) => ips.push(addr))
+	})
+	const ddnetLinks = ips.map((ip) => `ddnet://${ip.substr(13)}`)
+	return ddnetLinks
+}
+
+const sendHelpToChiler = async () => {
+	const links = await getServerIpsByPlayerName('ChillerDragon')
+	console.log(links)
+	if (links.length === 0) {
+		console.log('WARNOING chiler not foudn')
+		return 'chiler is in danger on a unknown tw server'
+	} else if (links.length === 1) {
+		return `send help to chiler by clickin on this link ${links[0]}`
+	}
+	return `chiler is one one of those servers ${links.join(', ')} send help to this poor soul in danger`
+}
+
+const client = new irc.Client('irc.ipv6.quakenet.org', 'chillerbot', {
+	channels: ['#ddnet'],
+})
+
+client.addListener('message#ddnet', async (from, message) => {
 	console.log(`<${from}> ${message}`)
 	if (message[0] !== '!') {
 		return
@@ -24,6 +60,8 @@ client.addListener('message#ddnet', (from, message) => {
 			client.say('#ddnet', 'only papa chiler can pinger.');
 			return
 		}
+		const helpTxt = await sendHelpToChiler()
+		const helpMsg = `${process.env.MOD_PING} ${await sendHelpToChiler()}`
 		client.say('#ddnet', process.env.MOD_PING);
 	}
 })
