501ce87d9bad407ca9f428077ae5edd0bcfd6296
ChillerDragon <ChillerDragon@gmail.com>
2023-04-15 17:53:26 +0200

    This is weird

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const client = new irc.Client('irc.ipv6.quakenet.org', 'chillerbot', {
 	channels: ['#ddnet'],
 })
 
+const interfaces = networkInterfaces()
+const eth0 = interfaces.eth0 ? interfaces.eth0.filter((a) => a.family === 'IPv4')[0].address : ''
+
+console.log(`eth0=${eth0}`)
+
 client.addListener('message#ddnet', (from, message) => {
 	console.log(`<${from}> ${message}`)
 	if (message[0] !== '!') {
@@ -13,8 +18,6 @@ client.addListener('message#ddnet', (from, message) => {
 	}
 	const cmd = message.substr(1)
 	if (cmd === 'help' || cmd === 'where' || cmd === 'info') {
-		const interfaces = networkInterfaces()
-		const localIp = interfaces.eth0 ? interfaces.eth0.filter((a) => a.family === 'IPv4')[0].address : ''
 		client.say('#ddnet', `https://github.com/ChillerDragon/ddnet-bot-irc eth0=${eth0} commands: !mods`);
 	} else if (cmd === 'mods' || cmd === 'mod' || cmd === 'moderator') {
 		if (from !== 'ChillerDragon') {
