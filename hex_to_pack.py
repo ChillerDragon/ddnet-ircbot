f227fca353ee79606f21b0e6c5b8e020aeab5c5d
ChillerDragon <ChillerDragon@gmail.com>
2023-04-28 18:18:10 +0200

    spam the shit out of this

--- hex_to_pack.py
+++ hex_to_pack.py
@@ -14,11 +14,20 @@ except ValueError:
     print('invalid hex')
     sys.exit(1)
 
-packet = 'error'
+# data = "02 7e 01 48 1f 93 d7 40 10 0a 80 01 6f 70 74 69 6f 6e 00 74 65 73 74 00 00 00"
+# data = bytearray.fromhex(data)
+
 try:
     packet = twnet_parser.packet.parse7(data)
 except:
-    pass
+    print('error')
+    sys.exit(1)
 
-print(packet)
+def code(msg):
+    print(f"```{msg}```")
+
+code(packet)
+code(packet.header)
+for msg in packet.messages:
+    code(msg)
 
