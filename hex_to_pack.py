595fa23d5262b93f521c4a65838e515e17a2de4b
ChillerDragon <ChillerDragon@gmail.com>
2023-05-07 14:36:42 +0200

    Support more hex formats

--- hex_to_pack.py
+++ hex_to_pack.py
@@ -3,20 +3,40 @@
 import sys
 import time
 
+import re
+import ast
+from binascii import hexlify
+import string
+
 import twnet_parser.packet
+
+def str_to_bytes(data):
+    data = data.strip()
+    if re.match('^b[\'\"].*[\'\"]$', data):
+        return ast.literal_eval(data)
+
+    if re.match('^0x', data):
+        data = data.replace('0x', '')
+
+    data = bytes(bytearray.fromhex(data))
+    return data
+
+# data = r"b'\x04\x00\x011\xe4\xc3\xd6\x00\x19\x030.7 802f1be60a05665f\x00\x00\x85\x1c'"
+# print(str_to_bytes(data))
+# data = "02 7e 01 48 1f 93 d7 40 10 0a 80 01 6f 70 74 69 6f 6e 00 74 65 73 74 00 00 00"
+# print(str_to_bytes(data))
+# data = "0x02 0xff 0x03"
+# print(str_to_bytes(data))
 
 if len(sys.argv) == 1:
     print("provide tw traffix hex like this: 04 0a 00 cf 2e de 1d 04")
     sys.exit(1)
 
 try:
-    data = bytearray.fromhex(sys.argv[1])
+    data = str_to_bytes(data)
 except ValueError:
     print('invalid hex')
     sys.exit(1)
-
-# data = "02 7e 01 48 1f 93 d7 40 10 0a 80 01 6f 70 74 69 6f 6e 00 74 65 73 74 00 00 00"
-# data = bytearray.fromhex(data)
 
 try:
     packet = twnet_parser.packet.parse7(data)
