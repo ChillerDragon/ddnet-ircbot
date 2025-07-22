34feb253f335e83993317060f4ab6864acccad0b
ChillerDragon <ChillerDragon@gmail.com>
2025-07-22 19:31:07 +0200

    jjajasvisi

--- src/qna.ts
+++ src/qna.ts
@@ -1,4 +1,4 @@
-export const answerToCommonQuestion = (message: String): String => {
+export const answerToCommonQuestion = (message: string): string => {
 	if(message.includes("clang") && message.includes("format") && message.includes("download")) {
 		return "you can download clang-format-10 here https://github.com/muttleyxd/clang-tools-static-binaries/releases"
 	}
