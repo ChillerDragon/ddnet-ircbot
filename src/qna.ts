22cf7339be1ce30ff6a242a8b3ba9dfa108e3780
ChillerDragon <ChillerDragon@gmail.com>
2025-07-22 19:40:01 +0200

    random ass protection xd
    if someone says you can download it here idk

--- src/qna.ts
+++ src/qna.ts
@@ -1,5 +1,5 @@
 export const answerToCommonQuestion = (message: string): string => {
-	if(message.includes("clang") && message.includes("format") && message.includes("download")) {
+	if(message.includes("clang") && message.includes("format") && message.includes("download") && !message.includes("https://github.com/muttleyxd/clang-tools-static-binaries/releases")) {
 		return "you can download clang-format-10 here https://github.com/muttleyxd/clang-tools-static-binaries/releases"
 	}
 	return ""
