00ed1f6ae4e851c2b35f99dc69a3963b08121ea6
ChillerDragon <chillerdragon@gmail.com>
2025-07-23 13:05:36 +0200

    empty test

--- src/test/test_msg_matchers.ts
+++ src/test/test_msg_matchers.ts
@@ -1,6 +1,7 @@
 import { strict as assert } from 'node:assert'
 import { getIssueUrls } from '../msg_matchers'
 
+assert.deepEqual(getIssueUrls(''), [])
 assert.deepEqual(getIssueUrls('#1'), ['https://github.com/ddnet/ddnet/issues/1'])
 assert.deepEqual(getIssueUrls('#2'), ['https://github.com/ddnet/ddnet/issues/2'])
 assert.deepEqual(getIssueUrls('#22'), ['https://github.com/ddnet/ddnet/issues/22'])
