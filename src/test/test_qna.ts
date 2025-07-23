8a6ddbc274b7d1d2182378d956954a8478f9ef2e
ChillerDragon <chillerdragon@gmail.com>
2025-07-23 12:48:19 +0200

    add test for qna

---
+++ src/test/test_qna.ts
@@ -0,0 +1,9 @@
+import { answerToCommonQuestion } from '../qna'
+import { strict as assert } from 'node:assert';
+
+const clang = "you can download clang-format-10 here https://github.com/muttleyxd/clang-tools-static-binaries/releases"
+
+assert.equal("", answerToCommonQuestion("something that should never hit"))
+assert.equal("", answerToCommonQuestion("you can download clang format here https://github.com/muttleyxd/clang-tools-static-binaries/releases"))
+assert.equal(clang, answerToCommonQuestion("where cna i has download clang formats nuts"))
+
