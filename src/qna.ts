d4e0566ce186ddac60715dba418307b7e0804d86
ChillerDragon <ChillerDragon@gmail.com>
2025-07-26 17:07:44 +0200

    where donate

--- src/qna.ts
+++ src/qna.ts
@@ -2,5 +2,12 @@ export const answerToCommonQuestion = (message: string): string => {
   if (message.includes('clang') && message.includes('format') && message.includes('download') && !message.includes('https://github.com/muttleyxd/clang-tools-static-binaries/releases')) {
     return 'you can download clang-format-10 here https://github.com/muttleyxd/clang-tools-static-binaries/releases'
   }
+  if (!message.includes('https://ddnet.org/funding/')) {
+    const where = message.includes('where') || message.includes('how') || message.includes('?')
+    const donate = message.includes('donate') || message.includes('money') || message.includes('cash') || message.includes('rich')
+    if (where && donate) {
+      return 'You can see donors and how to donate here https://ddnet.org/funding/'
+    }
+  }
   return ''
 }
--- src/test/test_qna.ts
+++ src/test/test_qna.ts
@@ -6,3 +6,6 @@ const clang = 'you can download clang-format-10 here https://github.com/muttleyx
 assert.equal(answerToCommonQuestion('something that should never hit'), '')
 assert.equal(answerToCommonQuestion('you can download clang format here https://github.com/muttleyxd/clang-tools-static-binaries/releases'), '')
 assert.equal(answerToCommonQuestion('where cna i has download clang formats nuts'), clang)
+
+const donate = 'You can see donors and how to donate here https://ddnet.org/funding/'
+assert.equal(answerToCommonQuestion('where donate i can???'), donate)
