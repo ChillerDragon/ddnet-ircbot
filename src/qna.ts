de4b3b5f07df339d3df871c556d9757109968faf
ChillerDragon <chillerdragon@gmail.com>
2025-07-30 12:15:44 +0200

    rich has too many heinrich false positives

--- src/qna.ts
+++ src/qna.ts
@@ -4,7 +4,7 @@ export const answerToCommonQuestion = (message: string): string => {
   }
   if (!message.includes('https://ddnet.org/funding/')) {
     const where = message.includes('where') || message.includes('how') || message.includes('?')
-    const donate = message.includes('donate') || message.includes('money') || message.includes('cash') || message.includes('rich')
+    const donate = message.includes('donate') || message.includes('money') || message.includes('cash')
     if (where && donate) {
       return 'You can see donors and how to donate here https://ddnet.org/funding/'
     }
--- src/test/test_qna.ts
+++ src/test/test_qna.ts
@@ -10,3 +10,4 @@ assert.equal(answerToCommonQuestion('where cna i has download clang formats nuts
 const donate = 'You can see donors and how to donate here https://ddnet.org/funding/'
 assert.equal(answerToCommonQuestion('where donate i can???'), donate)
 assert.equal(answerToCommonQuestion('You know what I liked? Heinrichs proposal to move all backcimpat to a completely separate module'), '')
+assert.equal(answerToCommonQuestion("I actually recently proposed this in the mod channel. In hopes that it'd scare heinrich into showing back up and finishing QUIC"), '')
