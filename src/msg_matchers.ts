abbebd5b22a9f5ed63766b6be264eb34e03f12c0
ChillerDragon <chillerdragon@gmail.com>
2025-08-02 14:48:55 +0200

    supply chain attack

--- src/msg_matchers.ts
+++ src/msg_matchers.ts
@@ -1,11 +1,18 @@
 export const getIssueUrls = (message: string): string[] => {
   const urls: string[] = []
-  const matches = message.match(new RegExp('#\\d+', 'g'))
+  const matches = message.match(new RegExp('[a-zA-Z0-9_-]*#\\d+', 'g'))
   if (matches == null) {
     return urls
   }
   matches.forEach((match) => {
-    const ghUrl = `https://github.com/ddnet/ddnet/issues/${match.substring(1)}`
+    const issueId = match.split('#')[1]
+    let repoUrl = 'https://github.com/ddnet/ddnet'
+    if (/(^|[^a-zA-Z0-9_-])(tater|tclient|t|tc)#/.test(match)) {
+      repoUrl = 'https://github.com/sjrc6/TaterClient-ddnet'
+    } else if (/(^|[^a-zA-Z0-9_-])(rs|rust|jup|jap|jop|pg)#/.test(match)) {
+      repoUrl = 'https://github.com/ddnet/ddnet-rs'
+    }
+    const ghUrl = `${repoUrl}/issues/${issueId}`
     urls.push(ghUrl)
   })
   return urls
